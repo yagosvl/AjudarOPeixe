b13d26918bbbb25729011dd443229b97fac5b2ba
LuizGuerra <luizpedrofg@gmail.com>
2019-03-27 16:09:51 -0300

    adicionada tela para criar tarefas (ainda não funciona)

--- AjudarOPeixe/AjudarOPeixe/ListaTarefasViewController.swift
+++ AjudarOPeixe/AjudarOPeixe/ListaTarefasViewController.swift
@@ -35,7 +35,10 @@ class ListaTarefasViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        //self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
